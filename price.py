af19410e434d33f6b95f23ef2621650faa2ca71a
blumenkrants <45210244+blumenkrants@users.noreply.github.com>
2019-09-27 17:59:10 +0300

    Delete price.py

--- price.py
+++
@@ -1,7 +0,0 @@
-price = int(56.24)
-one = "Цена: "
-two = " руб."
-def format_price(one, price, two):
-  return (str(one) + str(price) + str(two))
-display_price = format_price(one, price, two)
-print(display_price)
